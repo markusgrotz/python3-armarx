5cb1e37fe3024c4ebb10a41ff2ad62a07a5c7ce0
Markus Grotz <markus.grotz@kit.edu>
2018-10-02 16:15:17 +0200

    revise proxy_loader module

--- armarx/proxy_loader.py
+++ armarx/proxy_loader.py
@@ -4,6 +4,7 @@ from importlib.abc import MetaPathFinder
 
 import os
 from lxml import etree
+from collections import namedtuple
 
 from .interface_helper import load_armarx_slice
 from .cmake_helper import get_data_path
@@ -14,6 +15,10 @@ from .ice_manager import get_proxy
 from .ice_manager import get_topic
 
 logger = logging.getLogger(__name__)
+
+
+VariantInfo = namedtuple('VariantInfo', ['package_name', 'type_name',
+                                         'include_path', 'default_name'])
 
 
 class ArmarXVariantInfoFinder(MetaPathFinder):
@@ -43,21 +48,30 @@ class ArmarXVariantInfoFinder(MetaPathFinder):
             for nav in tree.xpath('//VariantInfo/Lib/' + t):
                 type_name = nav.get('typeName').split('::')[-1]
                 include_path = nav.get('include')[start_pos:-2] + '.ice'
-                mapping[type_name] = package_name, include_path
+                default_name = nav.get('propertyDefaultValue')
+                mapping[type_name] = VariantInfo(package_name, type_name, include_path, default_name)
         return mapping
 
     def find_spec(self, fullname, path, target=None):
         if not fullname.startswith('armarx.'):
             return None
         type_name = fullname.split('.')[1]
-        package_name, include_path = self.mapping.get(type_name, (None, None))
-        if package_name and include_path:
-            load_armarx_slice(package_name, include_path)
-            self.patch_slice_definition(type_name)
+        variant_info = self.mapping.get(type_name, None)
+        if variant_info:
+            load_armarx_slice(variant_info.package_name, variant_info.include_path)
+            self.patch_slice_definition(variant_info)
         return None
 
-    def patch_slice_definition(self, type_name):
+    def patch_slice_definition(self, variant_info):
+        default_name = variant_info.default_name
+
+        def get_default_topic(cls, name=None):
+            return get_topic(cls, name or default_name)
+
+        def get_default_proxy(cls, name=None):
+            return get_proxy(cls, name or default_name)
+
         mod = importlib.import_module('armarx')
-        cls = getattr(mod, type_name)
-        cls.get_proxy = types.MethodType(get_proxy, cls)
-        cls.get_topic = types.MethodType(get_topic, cls)
+        cls = getattr(mod, variant_info.type_name)
+        cls.get_proxy = types.MethodType(get_default_proxy, cls)
+        cls.get_topic = types.MethodType(get_default_topic, cls)
