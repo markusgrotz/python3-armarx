21c8726f8882af1e5d1910c624b40c42b825e342
Markus Grotz <markus.grotz@kit.edu>
2018-10-02 11:19:14 +0200

    add ArmarXArgumentParser

---
+++ armarx/parser.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+
+import logging
+
+
+class ArmarXArgumentParser(ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def parse_args(self):
+        verbose_group = self.add_mutually_exclusive_group()
+        verbose_group.add_argument('-v', '--verbose', action='store_true', help='be verbose')
+        verbose_group.add_argument('-q', '--quiet', action='store_true', help='be quiet')
+
+        args = super().parse_args()
+
+        log_format = '%(asctime)s - %(levelname)s - %(message)s'
+        if args.verbose:
+            logging.basicConfig(format=log_format, level=logging.DEBUG)
+        elif args.quiet:
+            logging.basicConfig(format=log_format, level=logging.ERROR)
+        else:
+            logging.basicConfig(format=log_format, level=logging.INFO)
+
+        return args
