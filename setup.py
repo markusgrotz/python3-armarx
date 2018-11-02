3e7a9d6e084a7e9ba5323dc3bf86def1e4796c22
Markus Grotz <markus.grotz@kit.edu>
2018-11-02 15:26:22 +0100

    increase version

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
-import __version__ as v
 # from pip.req import parse_requirements
 
 # install_reqs = parse_requirements("requirements.txt")
@@ -9,16 +8,16 @@ import __version__ as v
 # test_reqs = [str(i.req) for i in test_req]
 
 setup(
-    name="armarx",
-    version=v.armarx_version,
+    name="armarx-dev",
+    version="0.10.2",
     description="python bindings for ArmarX",
     author="Markus Grotz",
     author_email="markus.grotz@kit.edu",
     url="https://armarx.humanoids.kit.edu/",
     # install_requires=reqs,
     # tests_require=test_reqs,
-    install_requires=['argcomplete', 'configparser'],
-    tests_require=['nose', 'mock'],
+    # install_requires=['argcomplete', 'configparser'],
+    # tests_require=['nose', 'mock'],
     test_suite="nose.collector",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
