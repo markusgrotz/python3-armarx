33fe3941f29ac423879c735c84fc3eb1d2b1d365
Christoph Pohl <christoph.pohl@kit.edu>
2019-08-01 16:49:54 +0200

    fixed wrong ice data-type for XYZRGBL

--- armarx/data_types.py
+++ armarx/data_types.py
@@ -10,7 +10,7 @@ Point3D = np.dtype({"names": ["x", "y", "z"],
                   "formats": [np.float32, np.float32, np.float32]})
 OrientedPoint3D = np.dtype([("normal", Normal3D), ("point", Point3D)])
 ColoredPoint3D = np.dtype([("color", RGBA), ("point", Point3D)])
-ColoredLabeledPoint3D = np.dtype([("color", RGBA), ("point", Point3D), ("label", np.uint32)])
+ColoredLabeledPoint3D = np.dtype([("point", Point3D), ("color", RGBA), ("label", np.uint32)])
 # TODO: why is the label here np.uint32 and not np.uint8? Is it the same for the other labels?
 # http://docs.pointclouds.org/1.7.0/structpcl_1_1_label.html
 # pcl::Label is uint_32 --> most likely in the method ConvertToPCL in armarx the label is cast to this type and this
