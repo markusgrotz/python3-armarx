af5b7d94ed0edd811087e24e11ccc08a92af324e
Markus Grotz <markus.grotz@kit.edu>
2019-12-11 16:13:17 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from setuptools import setup, find_packages
-# from pip.req import parse_requirements
+from pip.req import parse_requirements
 
-# install_reqs = parse_requirements("requirements.txt")
-# test_req = parse_requirements("test_requirements.txt")
-# reqs = [str(i.req) for i in install_reqs]
-# test_reqs = [str(i.req) for i in test_req]
+install_reqs = parse_requirements('requirements.txt', session='_dummy_session')
+test_req = parse_requirements('test_requirements.txt', session='_dummy_session')
+reqs = [str(i.req) for i in install_reqs]
+test_reqs = [str(i.req) for i in test_req]
 
 setup(
-    name="armarx-dev",
-    version="0.10.6",
-    description="python bindings for ArmarX",
-    author="Markus Grotz",
-    author_email="markus.grotz@kit.edu",
-    url="https://armarx.humanoids.kit.edu/",
-    # install_requires=reqs,
-    # tests_require=test_reqs,
-    # install_requires=['argcomplete', 'configparser'],
-    # tests_require=['nose', 'mock'],
-    test_suite="nose.collector",
-    packages=find_packages(exclude=["tests"]),
+    name='armarx-dev',
+    version='0.10.6',
+    description='python bindings for ArmarX',
+    author='Markus Grotz',
+    author_email='markus.grotz@kit.edu',
+    url='https://armarx.humanoids.kit.edu/',
+    install_requires=reqs,
+    tests_require=test_reqs,
+    test_suite='nose.collector',
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
 )
