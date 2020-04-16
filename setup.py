b0efbc133397978274d51737ae1287e1599e711f
Christoph Pohl <christoph.pohl@kit.edu>
2020-04-16 10:35:30 +0200

    small changes to setup pip

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    
 install_reqs = parse_requirements('requirements.txt', session='_dummy_session')
 test_req = parse_requirements('test_requirements.txt', session='_dummy_session')
 reqs = [str(i.req) for i in install_reqs]
