52bf52ca063ff7abddb30267f4cad95d8fff4a1e
Rainer Kartmann <rainer.kartmann@kit.edu>
2021-06-21 11:55:06 +0200

    Revise / extend memory client classes

---
+++ armarx/armem/client/__init__.py
@@ -0,0 +1,5 @@
+from .Reader import Reader
+from .Writer import Writer
+from .MemoryNameSystem import MemoryNameSystem
+from .Commit import Commit, EntityUpdate
+from .MemoryListener import MemoryListener
