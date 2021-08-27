1c4a2cab0a2088df3e878b32d730de296cc6c97b
Markus Grotz <markus.grotz@kit.edu>
2021-08-27 10:49:06 +0200

    update ArmarXArgumentParser
    - add test connection
    - add doc string

--- armarx/parser.py
+++ armarx/parser.py
@@ -3,13 +3,28 @@ from argparse import ArgumentParser
 import sys
 import logging
 
+from .ice_manager import test_connection
 
 class ArmarXArgumentParser(ArgumentParser):
+    """
+    ArgumentParser with default arguments
+
+    ..see:: argparse.ArgumentParser
+
+    ..highlight:: python
+    ..codeblock:: python
+
+        from armarx.parser import ArmarXArgumentParser as ArgumentParser
+
+        parser = ArgumentParser('test')
+        parser.add_argument(...)
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def parse_args(self):
+        self.add_argument('--skip-connection-test', action='store_true', help='Do not test the connection')
         verbose_group = self.add_mutually_exclusive_group()
         verbose_group.add_argument('-v', '--verbose', action='store_true', help='be verbose')
         verbose_group.add_argument('-q', '--quiet', action='store_true', help='be quiet')
@@ -23,5 +38,8 @@ class ArmarXArgumentParser(ArgumentParser):
             logging.basicConfig(format=log_format, stream=sys.stdout, level=logging.ERROR)
         else:
             logging.basicConfig(format=log_format, stream=sys.stdout, level=logging.INFO)
+
+        if not args.skip_connection_test:
+            test_connection()
 
         return args
