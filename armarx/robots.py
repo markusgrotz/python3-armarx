63be26b4b3166afeff207de129d31f80d68c94f6
Markus Grotz <markus.grotz@kit.edu>
2021-09-12 21:35:09 +0200

    update robot module
    - organize imports
    - add documentation
    - revise code

--- armarx/robots.py
+++ armarx/robots.py
@@ -1,8 +1,7 @@
 import sys
 import logging
 import threading
-
-logger = logging.getLogger(__name__)
+import time
 
 from armarx import ice_manager
 
@@ -18,15 +17,19 @@ from armarx import EmergencyStopMasterInterfacePrx
 from armarx import EmergencyStopState
 from armarx.speech import TextStateListener
 
+logger = logging.getLogger(__name__)
 
 class Robot(object):
+    """
+    Convenience class 
+    """
 
     def __init__(self):
         self._text_state_listener = TextStateListener()
         self._text_state_listener.register()
 
 
-        self.platform = PlatformNavigatorInterfacePrx.get_proxy()
+        self.navigator = PlatformNavigatorInterfacePrx.get_proxy()
         self.gaze = GazeControlInterfacePrx.get_proxy()
         #self._fusion = ElasticFusionInterfacePrx.get_proxy()
 
@@ -37,7 +40,11 @@ class Robot(object):
         self._text_state_listener.say(text)
 
     def scan_scene(self):
-        pass
+        # self._fusion.reset()
+        for yaw in [-0.3, 0.0, 0.3]:
+            self.gaze.setYaw(yaw)
+            time.sleep(0.3)
+        self.gaze.setYaw(0.0)
 
     def stop(self):
         self.emergency_stop.setEmergencyStopState(EmergencyStopState.eEmergencyStopActive)
@@ -51,6 +58,16 @@ from armarx import ControlMode
 
 
 class A6(Robot):
+    """
+    ARMAR-6
+
+    .. highlight:: python
+    .. code-block:: python
+
+        from armarx.robots import A6
+        robot = A6()
+        robot.say('hello world')
+    """
 
     def __init__(self):
         super().__init__()
@@ -63,28 +80,48 @@ class A6(Robot):
         pass
 
     def open_hand(self, hand_name='left, right', shape_name=None):
+        """
+        Opens a hand or both hands
+        :param hand_name: the name of the hand
+        :param shape_name: the name of the hand shape
+        """
         shape_name = shape_name or 'Open'
         if 'left' in hand_name:
             self.left_hand.setShape(shape_name)
         if 'right' in hand_name:
             self.right_hand.setShape(shape_name)
+        if 'both' in hand_name:
+            self.left_hand.setShape(shape_name)
+            self.right_hand.setShape(shape_name)
 
     def close_hand(self, hand_name='left, right', shape_name=None):
+        """
+        Closes a hand or both hands
+        :param hand_name: the name of the hand
+        :param shape_name: the name of the hand shape
+        """
         shape_name = shape_name or 'Close'
         if 'left' in hand_name:
             self.left_hand.setShape(shape_name)
         if 'right' in hand_name:
             self.right_hand.setShape(shape_name)
+        if 'both' in hand_name:
+            self.left_hand.setShape(shape_name)
+            self.right_hand.setShape(shape_name)
 
 
     def init_pose(self):
-        joint_angles = { "ArmL1_Cla1": 0.036781, "ArmL2_Sho1": 0.839879, "ArmL3_Sho2":
-                0.111953, "ArmL4_Sho3": 0.178885, "ArmL5_Elb1": 1.317399,
-                "ArmL6_Elb2": -0.077956, "ArmL7_Wri1": 0.081407, "ArmL8_Wri2":
-                0.171840, "ArmR1_Cla1": -0.036818, "ArmR2_Sho1": -0.839400,
-                "ArmR3_Sho2": 0.111619, "ArmR4_Sho3": -0.179005, "ArmR5_Elb1":
-                1.319545, "ArmR6_Elb2": 0.078254, "ArmR7_Wri1": -0.081144,
-                "ArmR8_Wri2": 0.171887, "Neck_1_Yaw": 0.0, "Neck_2_Pitch": 0.2, "TorsoJoint": 0.5}
+        """
+        Sets the joint to a default pose
+        """
+        joint_angles = {"ArmL1_Cla1": 0.036781, "ArmL2_Sho1": 0.839879,
+                "ArmL3_Sho2": 0.111953, "ArmL4_Sho3": 0.178885, "ArmL5_Elb1":
+                1.317399, "ArmL6_Elb2": -0.077956, "ArmL7_Wri1": 0.081407,
+                "ArmL8_Wri2": 0.171840, "ArmR1_Cla1": -0.036818, "ArmR2_Sho1":
+                -0.839400, "ArmR3_Sho2": 0.111619, "ArmR4_Sho3": -0.179005,
+                "ArmR5_Elb1": 1.319545, "ArmR6_Elb2": 0.078254, "ArmR7_Wri1":
+                -0.081144, "ArmR8_Wri2": 0.171887, "Neck_1_Yaw": 0.0,
+                "Neck_2_Pitch": 0.2, "TorsoJoint": 0.5}
         self.move_joints(joint_angles)
 
     def move_joints(self, joint_angles):
