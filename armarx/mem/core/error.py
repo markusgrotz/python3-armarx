187a0dcda37dc8467c8c28a196e4e3fe6790499d
Rainer Kartmann <rainer.kartmann@kit.edu>
2021-09-20 10:22:12 +0200

    Update usage of armem interface

--- armarx/mem/core/error.py
+++ armarx/mem/core/error.py
@@ -10,7 +10,7 @@ class CouldNotResolveMemoryServer(ArMemError):
 
     def __init__(self, memory_id: MemoryID, msg: str = ""):
         super().__init__(
-            f"Could not resolve the memory name '{memory_id}'."
-            f"\nMemory server for '{memory_id}' is not registered."
+            f"Could not resolve the memory name {memory_id}."
+            f"\nMemory server for {memory_id} is not registered."
             + ("\n" + msg if msg else "")
         )
