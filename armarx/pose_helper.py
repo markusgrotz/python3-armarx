59e5436e18bb5087c8aede8cd4b6d2a0b6bfc8d0
Markus Grotz <markus.grotz@kit.edu>
2021-12-13 14:36:56 +0100

    update pose_helper module

--- armarx/pose_helper.py
+++ armarx/pose_helper.py
@@ -33,17 +33,37 @@ def convert_position_to_global(f: FramedPositionBase) -> np.ndarray:
     pose = FramedPoseBase(position=f, orientation=FramedOrientationBase(), frame=f.frame, agent=f.agent)
     return convert_pose_to_global(pose)
 
-
-def convert_pose_to_global(f: FramedPoseBase) -> np.ndarray:
+def convert_mat_to_global(pose: np.ndarray, frame: str) -> np.ndarray:
     robot_state = RobotStateComponentInterfacePrx.get_proxy()
     current_robot_state = robot_state.getSynchronizedRobot()
     robot_pose = current_robot_state.getGlobalPose()
-    robot_node = current_robot_state.getRobotNode(f.frame).getPoseInRootFrame()
-
-    transform = pose2mat(f)
+    robot_node = current_robot_state.getRobotNode(frame).getPoseInRootFrame()
     transform_robot_node_to_root = pose2mat(robot_node)
-
     transform_root_to_global = pose2mat(robot_pose)
 
-    return np.dot(transform_root_to_global, np.dot(transform_robot_node_to_root, transform))
+    return np.dot(transform_root_to_global, np.dot(transform_robot_node_to_root, pose))
+
+
+def convert_mat_to_root(pose: np.ndarray, frame: str) -> np.ndarray:
+    robot_state = RobotStateComponentInterfacePrx.get_proxy()
+    current_robot_state = robot_state.getSynchronizedRobot()
+    if frame == 'Global' or frame == 'armarx::Global':
+        robot_pose = current_robot_state.getGlobalPose()
+        robot_pose[:3, :3] = robot_pose.T
+        robot_pose[3, :3] = -1 * np.dot(robot_pose[:3, :3], robot_pose[3, :3])
+        return np.dot(robot_pose, pose) 
+    else:
+        robot_node = current_robot_state.getRobotNode(frame).getPoseInRootFrame()
+        transform_robot_node_to_root = pose2mat(robot_node)
+        return np.dot(transform_robot_node_to_root, pose)
+
+
+def convert_pose_to_global(f: FramedPoseBase) -> np.ndarray:
+    transform = pose2mat(f)
+    return convert_mat_to_global(transform, f.frame)
+
+def convert_pose_to_root(f: FramedPoseBase) -> np.ndarray:
+    transform = pose2mat(f)
+    return convert_mat_to_root(transform, f.frame)
+
 
