a39f1fb5933a10508d3a1240940e267743f79b8c
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-01-28 08:16:45 +0100

    Extend point cloud recorder

--- examples/record_point_clouds.py
+++ examples/record_point_clouds.py
@@ -1,43 +1,62 @@
 import datetime
 import logging
 import os.path
-import time
 
 import numpy as np
 
 from armarx.ice_manager import is_alive
 from visionx.pointcloud_processor import PointCloudReceiver
-from visionx.pointcloud_provider import PointCloudProvider, dtype_point_color_xyz
 
 logger = logging.getLogger(__name__)
 
-
 output_directory = os.path.expandvars("$HOME/recorded-point-clouds")
+
+name = "PointCloudRecorder"
+source_provider_name = (
+    # "OpenNIPointCloudProvider"
+    "DynamicSimulationDepthImageProvider"
+)
+
+max_fps = 30
 
 
 def main():
-    name = "PointCloudRecorder"
-    receiver = PointCloudReceiver(name, source_provider_name="OpenNIPointCloudProvider")
-    receiver.on_connect()
+    receiver = PointCloudReceiver(name=name, source_provider_name=source_provider_name)
 
-    # result_provider = PointCloudProvider(f"{name}Result", point_dtype=dtype_point_color_xyz, connect=True)
-    # on_connect is called in the constructor if connect=True
-    # result_provider.on_connect()
+    logger.info(f"Wait for point cloud provider '{receiver.source_provider_name}' ...")
+    receiver.on_connect(wait_for_provider=True)
 
-    start = datetime.datetime.now()
+    t_start = datetime.datetime.now()
+    t_latest = None
+    count = 0
+
+    directory = os.path.join(output_directory, str(t_start))
+    logger.info(f"Storing point clouds in '{directory}'.")
+    os.makedirs(directory, exist_ok=True)
+
     try:
         while is_alive():
             pc, info = receiver.wait_for_next_point_cloud()
+            now = datetime.datetime.now()
 
-            # The point cloud we get is read-only, so we need to copy before modifying
+            if t_latest is not None and (now - t_latest).total_seconds() < (1 / max_fps):
+                continue
+            else:
+                t_latest = now
+
+            # The point cloud we get is read-only, so we need to copy before modifying.
             new_pc: np.ndarray = np.copy(pc)
 
-            delta = datetime.datetime.now() - start
-            filename = f"pointcloud_{delta}.npy"
-            filepath = os.path.join(output_directory, filename)
-            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+            filename = f"pointcloud_{now - t_start}.npy"
+            filepath = os.path.join(directory, filename)
 
-            np.save(new_pc)
+            np.save(filepath, new_pc)
+            count += 1
+
+            print_step = 10 ** max(1, int(np.log10(count)))
+            if count % print_step == 0:
+                logger.info(f"Stored {count} point clouds (reporting each {print_step}) ...")
+
 
     except KeyboardInterrupt:
         logger.info("Shutting down.")
