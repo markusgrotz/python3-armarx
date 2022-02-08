3986be6f31ff67027fe0c8b2a2ff4f95f86ac3a0
Markus Grotz <markus.grotz@kit.edu>
2022-02-08 13:09:59 +0100

    update imports for arviz.layer

--- armarx/arviz/layer.py
+++ armarx/arviz/layer.py
@@ -1,39 +1,38 @@
 from typing import List, Union, Tuple
 
-from armarx import slice_loader
 
-slice_loader.load_armarx_slice("RobotAPI", "ArViz/Component.ice")
-import armarx.viz
+from armarx.viz.data import Element 
+from armarx.viz.data import LayerUpdate 
 
-from .elements import Element
+from .elements import Element as E
 
 
 class Layer:
 
-    def __init__(self, component: str, name: str, elements: List[Element] = ()):
+    def __init__(self, component: str, name: str, elements: List[E] = ()):
         self.component = component
         self.name = name
-        self.elements: List[Element] = list(elements)
+        self.elements: List[E] = list(elements)
 
     def clear(self):
         self.elements = []
 
-    def add(self, element: Union[Element, armarx.viz.data.Element]):
+    def add(self, element: Union[Element, E]):
         self.elements.append(element)
 
     @property
     def id(self) -> str:
         return "{}/{}".format(self.component, self.name)
 
-    def data(self) -> armarx.viz.data.LayerUpdate:
-        return armarx.viz.data.LayerUpdate(component=self.component, name=self.name,
+    def data(self) -> LayerUpdate:
+        return LayerUpdate(component=self.component, name=self.name,
                                     elements=list(map(self._get_element_data, self.elements)))
 
     @staticmethod
-    def _get_element_data(element: Union[Element, armarx.viz.data.Element]):
-        if isinstance(element, Element):
+    def _get_element_data(element: Union[Element, E]):
+        if isinstance(element, E):
             return element.get_ice_data()
-        elif isinstance(element, armarx.viz.data.Element):
+        elif isinstance(element, Element):
             return element
         else:
             raise TypeError("Expected viz element, but received object of class '{}'.".format(element.__class__))
@@ -41,7 +40,7 @@ class Layer:
 
     def __iadd__(self, elements):
         """Add an element or multiple elements to this layer's elements."""
-        if isinstance(elements, Element) or isinstance(elements, armarx.viz.data.Element):
+        if isinstance(elements, E) or isinstance(elements, Element):
             self.elements.append(elements)
         else:
             self.elements += elements
