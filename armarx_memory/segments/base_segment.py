20fe419b4e5ce05923fe3b5ecd2ddb76f611e7ff
Markus Grotz <markus.grotz@kit.edu>
2022-03-10 00:39:25 +0100

    add BaseReader and BaseWriter classes

--- armarx_memory/segments/base_segment.py
+++ armarx_memory/segments/base_segment.py
@@ -31,6 +31,47 @@ class BaseClient(ABC):
         return (self.core_segment_id.with_provider_segment_name(provider_name)
                 .with_entity_name(entity_name))
 
+
+
+
+
+class BaseReader(BaseClient, ABC):
+
+    def __init__(self, reader: Reader):
+        super().__init__()
+        self.reader = reader
+
+    def fetch_latest_instance(self, updated_ids: Optional[List[MemoryID]] = None):
+        """
+        Query the latest snapshot of the given updated IDs and
+        return its first instance.
+        """
+        if updated_ids is None:
+            memory = self.query_latest(self.core_segment_id)
+
+            latest_snapshot = None
+
+            core_seg = memory.coreSegments[self.core_segment_id.core_segment_name]
+            for prov_seg in core_seg.providerSegments.values():
+                for entity in prov_seg.entities.values():
+                    for snapshot in entity.history.values():
+                        if latest_snapshot is None:
+                            latest_snapshot = snapshot
+                        elif latest_snapshot.id.timestampMicroSeconds < snapshot.id.timestampMicroSeconds:
+                            latest_snapshot = snapshot
+        else:
+            for up_id in updated_ids:
+                assert self.core_segment_id.contains(up_id)
+
+            latest_snapshot_id = max(updated_ids, key=lambda i: i.timestamp_usec)
+            latest_snapshot = self.query_snapshot(latest_snapshot_id)
+
+        if not latest_snapshot:
+            return None
+
+        latest_instance = latest_snapshot.instances[0]
+        return latest_instance
+
     @classmethod
     def from_mns(cls, mns: MemoryNameSystem, wait=True):
         if wait:
@@ -39,6 +80,30 @@ class BaseClient(ABC):
             mns_proxy = mns.get_reader(cls.core_segment_id)
         return cls(mns_proxy)
 
+
+
+
+class BaseWriter(BaseClient, ABC):
+
+    def __init__(self, writer: Writer):
+        super().__init__()
+        self.writer = writer
+
+     
+    def commit(self, entity_id: MemoryID, time_created_usec=None, **kwargs):
+        from armarx_memory.aron.conversion import to_aron
+        commit = Commit()
+        commit.add(entity_id=entity_id, time_created_usec=time_created_usec,
+                   instances_data=[to_aron(**kwargs)])
+        return self.commit(commit)
+
+    @classmethod
+    def from_mns(cls, mns: MemoryNameSystem, wait=True):
+        if wait:
+            mns_proxy = mns.wait_for_writer(cls.core_segment_id)
+        else:
+            mns_proxy = mns.get_writer(cls.core_segment_id)
+        return cls(mns_proxy)
 
 
 
