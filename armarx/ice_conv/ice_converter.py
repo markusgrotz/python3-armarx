52dd3bd4fc29335e3121896397369fc80e223ccd
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-03-25 08:57:00 +0100

    Fix call to _import_dto

--- armarx/ice_conv/ice_converter.py
+++ armarx/ice_conv/ice_converter.py
@@ -83,7 +83,10 @@ class IceConverter(abc.ABC):
     def get_dto(cls):
         if cls._dto is None:
             # Dunno why yet, but we need to pass cls explicitly here ...
-            cls._dto = cls._import_dto(cls)
+            try:
+                cls._dto = cls._import_dto(cls)
+            except TypeError:
+                cls._dto = cls._import_dto()
         return cls._dto
 
     @abc.abstractmethod
