803dcd81965dcc7202ca67e64ac2eda56f876f52
Rainer Kartmann <rainer.kartmann@kit.edu>
2022-09-08 13:43:41 +0200

    Refactor nd array conversion

--- armarx_memory/aron/conversion/pythonic_from_to_ice.py
+++ armarx_memory/aron/conversion/pythonic_from_to_ice.py
@@ -39,8 +39,7 @@ def pythonic_to_aron(value) -> "armarx.aron.data.dto.GenericData":
         return value
 
     elif isinstance(value, np.ndarray):
-        shape = (*value.shape, value.itemsize)
-        return AronIceTypes.NDArray(shape=shape, type=str(value.dtype), data=value.tobytes())
+        return ndarray_to_aron(value)
 
     try:
         return value.to_aron()
@@ -73,40 +72,7 @@ def pythonic_from_aron(data: "armarx.aron.data.dto.GenericData"):
         return data
 
     if isinstance(data, AronIceTypes.NDArray):
-        # Last entry is #bytes per entry
-        byte_data: bytes = data.data
-
-        shape: ty.Tuple[int]
-        try:
-            shape = data.dimensions[:-1]
-        except AttributeError:
-            shape = data.shape[:-1]
-        shape = tuple(shape)
-
-        dtype = dtypes_dict.get(data.type, None)
-        if dtype is None:
-            size = np.product(shape)
-            if size == 0:
-                dtype = np.uint8
-            else:
-                dtype_size = len(byte_data) // size
-                dtype_dict = {
-                    1: np.uint8,
-                    2: np.uint16,
-                    4: np.uint32,
-                    8: np.uint64
-                }
-                dtype = dtype_dict.get(dtype_size, None)
-                if dtype is None:
-                    # Build a structured dtype with sequence of bytes.
-                    dtype = np.dtype([("bytes", np.uint8, dtype_size)])
-
-            print(f"Unknown type '{data.type}' of array with shape {shape} and {len(byte_data)} bytes. "
-                  f"Falling back to {dtype}.")
-
-        array: np.ndarray = np.frombuffer(buffer=byte_data, dtype=dtype)
-        array = array.reshape(shape)
-        return array
+        return ndarray_from_aron(data)
 
     try:
         return data.value
@@ -128,3 +94,45 @@ def pythonic_from_aron(data: "armarx.aron.data.dto.GenericData"):
 
     raise TypeError(f"Could not handle aron object of type '{type(data)}'.\n"
                     f"dir(a): {dir(data)}")
+
+
+def ndarray_to_aron(value: np.ndarray) -> AronIceTypes.NDArray:
+    shape = (*value.shape, value.itemsize)
+    return AronIceTypes.NDArray(shape=shape, type=str(value.dtype), data=value.tobytes())
+
+
+def ndarray_from_aron(data: AronIceTypes.NDArray) -> np.ndarray:
+    # Last entry is #bytes per entry
+    byte_data: bytes = data.data
+
+    shape: ty.Tuple[int]
+    try:
+        shape = data.dimensions[:-1]
+    except AttributeError:
+        shape = data.shape[:-1]
+    shape = tuple(shape)
+
+    dtype = dtypes_dict.get(data.type, None)
+    if dtype is None:
+        size = np.product(shape)
+        if size == 0:
+            dtype = np.uint8
+        else:
+            dtype_size = len(byte_data) // size
+            dtype_dict = {
+                1: np.uint8,
+                2: np.uint16,
+                4: np.uint32,
+                8: np.uint64
+            }
+            dtype = dtype_dict.get(dtype_size, None)
+            if dtype is None:
+                # Build a structured dtype with sequence of bytes.
+                dtype = np.dtype([("bytes", np.uint8, dtype_size)])
+
+        print(f"Unknown type '{data.type}' of array with shape {shape} and {len(byte_data)} bytes. "
+              f"Falling back to {dtype}.")
+
+    array: np.ndarray = np.frombuffer(buffer=byte_data, dtype=dtype)
+    array = array.reshape(shape)
+    return array
