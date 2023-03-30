37e332a010c0bbb269d3db77d14ef692db7629e1
Jonas Lewandrowski <joecarverde@gmail.com>
2023-03-30 12:29:46 +0200

    Added redirect python scripts for arviz

---
+++ armarx/arviz/elements/elements.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.arviz.elements.elements' instead.", DeprecationWarning
+)
+
+from armarx_core.arviz.elements.elements import *
---
+++ armarx/arviz/elements/mesh.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.arviz.elements.mesh' instead.", DeprecationWarning
+)
+
+from armarx_core.arviz.elements.mesh import *
