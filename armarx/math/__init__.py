fb9ca74cd5d7448499fafd338e12dc32d971ab25
Jonas Lewandrowski <joecarverde@gmail.com>
2023-03-30 12:45:51 +0200

    Math redirects

--- armarx/math/__init__.py
+++ armarx/math/__init__.py
@@ -3,3 +3,5 @@ import warnings
 warnings.warn(
     "Use 'armarx_core.math' instead.", DeprecationWarning
 )
+
+from armarx_core.math.rescale import rescale
---
+++ armarx/math/spherical.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.math.spherical' instead.", DeprecationWarning
+)
+
+from armarx_core.math.spherical import *
