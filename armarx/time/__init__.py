7043d11ae7a8d1eedde3915855c09e9798c0d584
Jonas Lewandrowski <joecarverde@gmail.com>
2023-03-30 12:50:29 +0200

    Tools and time redirects

---
+++ armarx/time/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.time' instead.", DeprecationWarning
+)
+
+import armarx_core.time
---
+++ armarx/tools/metronome.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.tools.metronome' instead.", DeprecationWarning
+)
+
+from armarx_core.tools.metronome import *
