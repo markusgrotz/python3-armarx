50fd598c849d8a18b9d3dadb3fb738565baf5cb8
Jonas Lewandrowski <joecarverde@gmail.com>
2023-04-11 15:46:02 +0200

    Fix init.py of armarx.arviz

---
+++ armarx/arviz/elements/__init__.py
@@ -0,0 +1,5 @@
+warnings.warn(
+    "Use 'armarx_core.arviz' instead.", DeprecationWarning
+)
+
+from armarx_core.arviz.elements.mesh import Mesh
---
+++ armarx/math/transform.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Use 'armarx_core.math.transform' instead.", DeprecationWarning
+)
+
+from armarx_core.math.transform import *
