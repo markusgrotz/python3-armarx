2704f3760e49734ba3b9829e121619a9063ab6ec
Jianfeng Gao <jianfeng.gao@kit.edu>
2023-04-18 17:17:56 +0200

    revert change

--- armarx_memory/aron/conversion/dataclass_from_to_pythonic.py
+++ armarx_memory/aron/conversion/dataclass_from_to_pythonic.py
@@ -243,7 +243,7 @@ class DataclassFromToDict:
             if self.logger is not None:
                 self.logger.debug(f"{pre}> Process NoneType")
 
-            # assert type_ == type(None), type_
+            assert type_ == type(None), type_  # Note, If this fails, comment out
             return None
 
         elif isinstance(value, list):
