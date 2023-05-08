4326c5e69062846ccf6d927c23b53d9e0f4267d3
armar-user <armar6@kit>
2023-05-08 10:38:07 +0200

    examples/execute_skill (committed by FR)

---
+++ examples/execute_skill.py
@@ -0,0 +1,27 @@
+import armarx
+from armarx_skills.provider.skill_id import SkillID
+from armarx_skills.manager.skill_manager import SkillManager
+from armarx_skills.manager.skill_execution_request import SkillExecutionRequest
+
+
+if __name__ == '__main__':
+
+    name = "execute_skill.py"
+
+    skill_manager = SkillManager.wait_for_manager()
+    skill_manager.proxy.ice_ping()
+
+    # skill_id = SkillID("introduction_skill_provider", "IntroduceYourself")
+    # skill_id = SkillID("introduction_skill_provider", "IntroduceYourselfAtLocation")
+
+    # skill_manager.execute_skill_with_default_params(executor_name=name, skill_id=skill_id)
+
+    skill_id = SkillID("bring_object_from_kitchen_to_human", "BringObjectFromKitchenToHuman")
+    skill_manager.execute_skill(SkillExecutionRequest(
+        executor_name=name,
+        skill_id=skill_id,
+        params={
+            "objectPhrase": "apple tea",
+            "utterance": "bring me the apple tea from the kitchen",
+        }
+    ))
