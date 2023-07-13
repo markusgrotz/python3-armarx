2fa89445261acf7dff56ac5394d9fca3bf160e0a
Rainer Kartmann <rainer.kartmann@kit.edu>
2023-07-13 09:02:26 +0200

    Add __str__ and to_str() functions to Skill_id

--- armarx_skills/provider/skill_id.py
+++ armarx_skills/provider/skill_id.py
@@ -4,11 +4,24 @@ from armarx_skills.provider import dto
 
 from armarx_memory.ice_conv.ice_converter import IceConverter
 
+PREFIX_SEPARATOR = "->"
+NAME_SEPARATOR = "/"
+
 
 @dc.dataclass
 class SkillID:
     provider_name: str
     skill_name: str
+
+    def to_str(self, prefix: str):
+        # # return (prefix.empty() ? std::string("") : (prefix + PREFIX_SEPARATOR)) + providerName + NAME_SEPARATOR + skillName;
+        return "".join([("" if len(prefix) == 0 else (prefix + PREFIX_SEPARATOR)),
+                        self.provider_name, NAME_SEPARATOR, self.skill_name])
+
+    def __str__(self):
+        # return (prefix.empty() ? std::string("") : (prefix + PREFIX_SEPARATOR)) + providerName + NAME_SEPARATOR + skillName;
+
+        return f"{self.skill_name}"
 
 
 class SkillIdConv(IceConverter):
